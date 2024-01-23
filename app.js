8ac16e3900944cf3bdb07ea227426d6c5a372191
Saleh Basalim <sbasalim@una.edu>
2024-01-22 20:13:46 -0600

    added port binding

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
-app.get('/', function (req, res) {
-  res.send('Hello World form Express')
+app.get('/', (req, res) => {
+  res.send('Hello World From Express')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`mike app listening on port ${port}`)
+})
